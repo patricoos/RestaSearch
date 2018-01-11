57973f135d010d712fb050b7c2718a20f89afb56
BUJALLA-PATRYK\Patryk Bujalla <patricoos@gmail.com>
2018-01-12 00:02:28 +0100

    publikowanie

--- RestaSearch/RestaSearch/Controllers/LokaleController.cs
+++ RestaSearch/RestaSearch/Controllers/LokaleController.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using RestaSearch.Models;
 
 namespace RestaSearch.Controllers
 {
-    public class LokaleController : Controller
-    {
+	public class LokaleController : Controller
+	{
 		private RSContext db = new RSContext();
 		// GET: Lokale
 		public ActionResult Index()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 		public ActionResult Lista(string nazwaKategorii)
 		{
 			var kategoria = db.Kategorie.Include("Lokale").Where(k => k.NazwaKategorii.ToUpper() == nazwaKategorii.ToUpper()).Single();
@@ -47,5 +49,28 @@ namespace RestaSearch.Controllers
 			var lokale = db.Lokale.Where(a => !a.Ukryty && a.NazwaLokalu.ToLower().Contains(term.ToLower())).ToList();
 			return View("Lista", lokale);
 		}
+
+
+
+
+
+		//public ActionResult LokaleSzukajLokalizacja(string y, string x)
+		//{
+		//	double lokUzWys = Konwert(y);
+		//	double lokUzSzer = Konwert(x);
+		//	double wektor;
+
+		//	var lokale = db.Lokale.Where(a => !a.Ukryty).ToList();
+		//	lokale.Add(new Lokal() { NazwaKategorii = "crank arm", KategoriaId = 1234 });
+
+
+		//	return View("Lista", lokale);
+		//}
+
+		//public double Konwert(string value) {
+		//	double lok = Convert.ToDouble(value);
+		//	return lok;
+		//}
+
 	}
 }
